f38646b13cbbbba816cbe0320b08367cf8a14447
HenriqueGreghi <92762708+HenriqueGreghi@users.noreply.github.com>
2022-08-04 16:03:05 -0300

    add commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoDeRaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, x, raio;
+            n = 3.14159;
+
+            Console.WriteLine("Digite o raio:");
+            x = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            raio = n * (x * x);
+            Console.WriteLine("O resultado é : " + raio.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+
+    }
+}
